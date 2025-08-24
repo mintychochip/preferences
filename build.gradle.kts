b5c3b782b5558de2b84e19e94faef4438c55bde9
mintychochip <jlo2@csub.edu>
2025-08-23 17:07:20 -0700

    gradle

---
+++ build.gradle.kts
@@ -0,0 +1,52 @@
+plugins {
+    id("xyz.jpenilla.run-paper") version "2.3.1"
+    id("com.github.johnrengelman.shadow") version "8.1.1"
+    java
+    `java-library`
+    `maven-publish`
+}
+
+group = "net.aincraft"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+    maven("https://repo.papermc.io/repository/maven-public/")
+}
+
+java {
+    toolchain.languageVersion.set(JavaLanguageVersion.of(21))
+}
+
+dependencies {
+    compileOnly("io.papermc.paper:paper-api:1.21.7-R0.1-SNAPSHOT")
+    implementation("com.google.inject:guice:7.0.0")
+    implementation("com.zaxxer:HikariCP:5.1.0")
+}
+
+tasks {
+    shadowJar {
+        mergeServiceFiles()
+    }
+
+    build {
+        dependsOn(shadowJar)
+    }
+
+    named<xyz.jpenilla.runpaper.task.RunServer>("runServer") {
+        val toolchains = project.extensions.getByType<JavaToolchainService>()
+        javaLauncher.set(
+            toolchains.launcherFor {
+                languageVersion.set(JavaLanguageVersion.of(21))
+            }
+        )
+        minecraftVersion("1.21.7")
+        downloadPlugins {
+            url("https://github.com/EssentialsX/Essentials/releases/download/2.21.2/EssentialsX-2.21.2.jar")
+            url("https://www.spigotmc.org/resources/lwc-extended.69551/download?version=557109")
+            url("https://www.spigotmc.org/resources/vault.34315/download?version=344916")
+            url("https://www.spigotmc.org/resources/placeholderapi.6245/download?version=541946")
+        }
+
+    }
+}
